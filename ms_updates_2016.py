3d99768cfa7a6aa000991dd4118964daf456c9e1
Jon <37190282+systemicadm@users.noreply.github.com>
2020-05-15 23:16:04 -0400

    Update ms_updates_2016.py

--- ms_updates_2016.py
+++ ms_updates_2016.py
@@ -3,7 +3,7 @@ NewsFeed = feedparser.parse("https://support.microsoft.com/app/content/api/conte
 entry = NewsFeed.entries[1]
 
 print = entry.keys()
-
+print = 'hello world'
 #import feedparser
 
 #NewsFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
