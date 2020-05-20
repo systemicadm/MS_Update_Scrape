d46cce68698e26da739017276b826604009de396
Jon <37190282+systemicadm@users.noreply.github.com>
2020-05-19 22:29:28 -0400

    Update ms_updates_2016.py

--- ms_updates_2016.py
+++ ms_updates_2016.py
@@ -1,16 +1,15 @@
-import feedparser
-UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
-entry = UpdateFeed.entries[1]
-
-print = entry.keys()
 #import feedparser
-
 #UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
+#entry = UpdateFeed.entries[0]
 
-#entry = UpdateFeed.entries[1]
+#print = entry.keys()
+import feedparser
 
-#print entry.published
-#print "******"
-#print entry.summary
-#print "------Update Link--------"
-#print entry.link
+UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
+
+entry = UpdateFeed.entries[0]
+
+print = entry['title']
+print = "------Update Link--------"
+print = entry['link']
+print = "******"
