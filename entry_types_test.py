de0e0f39d3f0f44bfa42ec6af334cc0dae448b53
Jon <37190282+systemicadm@users.noreply.github.com>
2020-05-23 21:55:19 -0400

    reorg'd again

--- entry_types_test.py
+++ entry_types_test.py
@@ -1,10 +1,5 @@
 import feedparser
-
 UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
-
 entry = UpdateFeed.entries[0]
 
-print = entry['title']
-print = "------Update Link--------"
-print = entry['link']
-print = "******"
+print = entry.keys()
