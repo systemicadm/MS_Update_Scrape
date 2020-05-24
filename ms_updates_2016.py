a5c28d78a90a47caf204cae3a01ca348f0a8a980
Jon <37190282+systemicadm@users.noreply.github.com>
2020-05-23 21:48:08 -0400

    reorg

--- ms_updates_2016.py
+++ ms_updates_2016.py
@@ -1,8 +1,3 @@
-#import feedparser
-#UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
-#entry = UpdateFeed.entries[0]
-
-#print = entry.keys()
 import feedparser
 
 UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
@@ -13,3 +8,9 @@ print = entry['title']
 print = "------Update Link--------"
 print = entry['link']
 print = "******"
+
+#import feedparser
+#UpdateFeed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/rss")
+#entry = UpdateFeed.entries[0]
+
+#print = entry.keys()
